8c6d045af9152fe6baa5d273545869e759f357b0
Krylov_AY <moscow_city231@rambler.ru>
2024-05-24 15:32:48 +0300

    add adapter

---
+++ src/main/java/ru/javawebinar/basejava/util/LocalDateAdapters.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.basejava.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class LocalDateAdapters extends TypeAdapter<LocalDate> {
+
+    @Override
+    public void write(JsonWriter out, LocalDate value) throws IOException {
+        if (value == null) {
+            out.nullValue();
+        } else {
+            out.value(value.toString());
+        }
+    }
+
+    @Override
+    public LocalDate read(JsonReader in) throws IOException {
+        if (in.peek() == com.google.gson.stream.JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        } else {
+            return LocalDate.parse(in.nextString());
+        }
+    }
+}
